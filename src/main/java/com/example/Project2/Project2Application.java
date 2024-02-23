c73e2770b72720af36c5b671a880bc09ceaf50c5
Utsav Roy <utsavroy44@gmail.com>
2024-02-23 21:05:26 +0530

    Creating Student class, StudentService and StudentController classes

--- src/main/java/com/example/Project2/Project2Application.java
+++ src/main/java/com/example/Project2/Project2Application.java
@@ -1,15 +1,23 @@
 package com.example.Project2;
 
+import com.example.Project2.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 public class Project2Application {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(Project2Application.class, args);
-		//Checking pull command
-		System.out.println("Hello World");
 	}
+
+
 
 }
